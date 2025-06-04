dd7d01f6335f984e245b1300b5b8485426549066
bemagnodev <bemagno13@gmail.com>
2025-06-03 22:41:57 -0300

    Add solution for B_Kevin_and_Permutation.cpp (Codeforces)

---
+++ Codeforces/B_Kevin_and_Permutation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Otimizações de I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Seu código começa aqui!
+    // ...
+    // use "\n"
+    return 0;
+}
