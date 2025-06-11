7cc69ba512c01cb629a6cd86e38815ba90fa2f91
bemagnodev <bemagno13@gmail.com>
2025-06-11 18:09:53 -0300

    Add solution for acelerador.cpp (Uncategorized)

---
+++ Uncategorized/acelerador.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int d; cin >> d;
+    int r = ((d-3)%8)-2;
+    cout << r << endl;
+
+
+    return 0;
+}
