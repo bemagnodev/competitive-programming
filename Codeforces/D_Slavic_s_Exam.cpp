4ac2c7ff0692681a0bdd0124652b58f2f7fb3056
bemagnodev <bemagno13@gmail.com>
2025-07-10 10:48:36 -0300

    Add solution for D_Slavic_s_Exam.cpp (Codeforces)

---
+++ Codeforces/D_Slavic_s_Exam.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int T; cin >> T;
+
+    while(T--){
+        string s1, t, s2;
+        cin >> s1 >> t;
+
+
+    }
+
+    
+
+
+}
