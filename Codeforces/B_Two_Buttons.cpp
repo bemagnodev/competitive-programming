f7c89acec2ee3b6137750f09dd426c70e347ad29
bemagnodev <bemagno13@gmail.com>
2025-08-01 11:43:32 -0300

    Add solution for B_Two_Buttons.cpp (Codeforces)

---
+++ Codeforces/B_Two_Buttons.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int t; cin >> t;
+
+    while(t--){
+        ll n; cin >> n;
+    
+        for( int i = 0; i < n; i++){
+            
+        }
+        cout << endl;
+    }
+    
+    return 0;
+
+}
