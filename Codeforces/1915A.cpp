1308f4d215a89bd6b54722ddbdd7e0845eff4231
bemagnodev <bemagno13@gmail.com>
2025-10-18 19:28:49 -0300

    Add solution for 1915A.cpp (Codeforces)

---
+++ Codeforces/1915A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+ 
+
+void solve() {
+    int a, b ,c ; cin >> a >>b  >> c;
+    
+    cout << (a^b^c) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t =1; 
+    cin >> t;  
+
+    while (t--) {
+        solve();  
+    }
+
+    return 0;
+}
