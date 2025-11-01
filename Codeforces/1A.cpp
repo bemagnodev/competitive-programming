85dc283e52b81f9575dc847e840e0f0cf576dfed
bemagnodev <bemagno13@gmail.com>
2025-11-01 18:58:09 -0300

    Add solution for 1A.cpp (Codeforces)

---
+++ Codeforces/1A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int n,m; cin >> n >>m;
+    int l =n/2, h=m/2;
+
+    FOR(i,0,n){
+        FOR(j,0,m){
+            
+        }
+    }
+    
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t =1; 
+    cin >> t;  
+
+    while (t--) {
+        solve();  
+    }
+
+    return 0;
+}
