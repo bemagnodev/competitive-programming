0f6627aef9ad23a2633bae872b74c7eee141471b
bemagnodev <bemagno13@gmail.com>
2025-11-05 15:13:22 -0300

    Add solution for 1324A.cpp (Codeforces)

---
+++ Codeforces/1324A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+
+    int n; cin >> n;
+    vi a(n); 
+    int maior= 0;
+    FOR(i,0,n) {
+        cin >> a[i];
+    }
+    bool ok = true;
+    FOR(i,1,n) {
+        if(a[i]%2 != a[0]%2){
+            cout << "NO" << endl;
+            return;
+        }
+    }
+        cout << "YES" << endl;
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t =1; 
+    cin >> t;  
+
+    while (t--) {
+        solve();  
+    }
+
+    return 0;
+}
