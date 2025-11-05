e80afb30728ed0f04414e276c2b4686a134c8447
bemagnodev <bemagno13@gmail.com>
2025-11-05 14:35:41 -0300

    Add solution for 451A.cpp (Codeforces)

---
+++ Codeforces/451A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int n,m; cin >>n >> m;
+    int menor;
+    if(n>m) menor = m;
+    else menor = n;
+    if(menor%2==0){
+        cout << "Malvika" << endl;
+    }
+    else{
+        cout << "Akshat" << endl;
+
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t =1; 
+    // cin >> t;  
+
+    while (t--) {
+        solve();  
+    }
+
+    return 0;
+}
