dac2b6c623d0091dca8d957d1ed59b525e233e66
bemagnodev <bemagno13@gmail.com>
2025-11-12 12:34:35 -0300

    Add solution for 32B.cpp (Codeforces)

---
+++ Codeforces/32B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pair<int, int>>;
+
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    string s; cin >> s;
+    string a, b, ans;
+    // --
+    FOR(i,0, s.size()){
+        if(s [i] =='.') cout << "0";
+        else if(s[i+1]== '.') {
+            cout<< "1";
+            i++;
+        }
+        else {
+            cout << "2";
+            i++;
+        }
+
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t =1; 
+    // cin >> t;  
+
+    while (t--) {
+        solve();  
+    }
+
+    return 0;
+}
