6216a2327265db28ba6750da4e55eaba8f80fa91
bemagnodev <bemagno13@gmail.com>
2025-11-12 20:01:31 -0300

    Add solution for 272A.cpp (Codeforces)

---
+++ Codeforces/272A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int n; cin >>n;;
+    int soma = 0;
+    FOR(i,0,n){
+        int x; cin >>x; soma+=x;
+    }
+    n++;
+    ll ans =0;
+    FOR(i,1,6){
+        if((soma+i) % n !=1){
+            // deb(i);
+           ans++;
+        }
+    }
+    cout << ans <<endl;
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
