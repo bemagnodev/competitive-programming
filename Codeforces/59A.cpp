21c3af7341f5d39808195f98dbc2cbc1443e611a
bemagnodev <bemagno13@gmail.com>
2025-11-14 08:43:34 -0300

    Add solution for 59A.cpp (Codeforces)

---
+++ Codeforces/59A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    string s, low, up; cin >>  s;
+    for(char c : s){
+        low += tolower(c);
+        up += toupper(c);
+    }
+    int DIFF =0, diff= 0;
+    FOR(i,0, s.size()){
+        if(s[i] != low[i]) diff++;
+        else DIFF++; 
+    }
+    if(DIFF < diff) cout << up << endl;
+    else cout << low << endl;
+
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
