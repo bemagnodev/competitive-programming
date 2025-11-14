6cc0c293670cee0e00e694423f7efa08275a66f0
bemagnodev <bemagno13@gmail.com>
2025-11-14 08:49:22 -0300

    Add solution for 110A.cpp (Codeforces)

---
+++ Codeforces/110A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    string s; cin >> s;
+    int l= 0;
+    FOR(i,0, s.size()){
+        if(s[i] == '4' || s[i] == '7'){
+            l++;
+        }
+    }
+    // deb(l);
+    if(l == 4 || l == 7){
+        cout <<"YES\n";
+        return;
+    }
+    cout <<"NO\n";
+    
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
