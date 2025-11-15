d6ec7823d3c499482730e95a1264d36223f4fcba
bemagnodev <bemagno13@gmail.com>
2025-11-15 17:48:15 -0300

    Add solution for 133A.cpp (Codeforces)

---
+++ Codeforces/133A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    string s;cin >> s;
+    for(char c: s){
+        if(c =='H' || c == 'Q'|| c == '9'){
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
