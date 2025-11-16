c12d92a9edf565149465feb80312bac8e2962d29
bemagnodev <bemagno13@gmail.com>
2025-11-16 11:21:32 -0300

    Add solution for 148A.cpp (Codeforces)

---
+++ Codeforces/148A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int k,l,m,n,d; cin >> k >>l >>m>>n >> d;
+    ll ans = 0;
+    FOR(i,1,d+1){
+        if(i%k==0)ans++;
+        else if(i%l==0)ans++;
+        else if(i%m==0)ans++;
+        else if(i%n==0)ans++;
+    }
+    cout << ans << endl;
+
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
