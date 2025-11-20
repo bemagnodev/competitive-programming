e35d95fe121d8ee76cc50cb88d7e9fa8f550b955
bemagnodev <bemagno13@gmail.com>
2025-11-20 15:29:59 -0300

    Add solution for 2171A.cpp (Codeforces)

---
+++ Codeforces/2171A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int n ; cin >> n;
+    ll ans = 0;
+    if(n%2==0)
+    {
+        ans = ((n/4)+1);
+        if(n==2) ans = 1;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fastio;
+    int t =1; 
+     cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
