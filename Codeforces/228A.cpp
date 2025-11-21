5c2f8dd25756e3c668be095a96a0bfb08135a72c
bemagnodev <bemagno13@gmail.com>
2025-11-21 15:31:11 -0300

    Add solution for 228A.cpp (Codeforces)

---
+++ Codeforces/228A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    set<int> a;
+    FOR(i,0,4){
+        int x;cin >>x; a.insert(x);
+    }
+    cout << 4- a.size() << endl;
+
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
