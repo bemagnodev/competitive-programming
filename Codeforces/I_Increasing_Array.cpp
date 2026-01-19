4ee76a54f97caf4095ce1e913ca38fee7fde1815
bemagnodev <bemagno13@gmail.com>
2026-01-19 08:41:13 -0300

    Add solution for I_Increasing_Array.cpp (Codeforces)

---
+++ Codeforces/I_Increasing_Array.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int n; cin >> n;
+    vi a(n);
+    FOR(i,0,n){
+        cin >> a[i];
+    }
+    ll ans = 0;
+    FOR(i,1,n){
+        if(a[i]>= a[i-1]) continue;
+        int dif = a[i-1]-a[i];
+        ans += dif;
+        a[i] +=dif;
+    }
+    cout << ans << endl;
+
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
