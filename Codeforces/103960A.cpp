b74efbd0f8a31071461212ab4ac827c5f33a231a
bemagnodev <bemagno13@gmail.com>
2026-01-22 21:54:15 -0300

    Add solution for 103960A.cpp (Codeforces)

---
+++ Codeforces/103960A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+#define pb push_back
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    string s;
+    int n; cin >> n >> s;
+    s = "b"+ s+"b"; n+=2;
+    ll ans = 0;
+    FOR(i,1,n){
+        if(s[i]=='a'){
+            ans++;
+            if( s[i-1] =='b' && s[i+1]=='b'){
+                ans--;
+            }
+        }
+    }
+    cout << ans<<endl;
+
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
