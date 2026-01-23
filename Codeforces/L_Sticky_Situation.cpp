985656ade745bab271d59fd5282df4d9bdb619b8
bemagnodev <bemagno13@gmail.com>
2026-01-23 09:28:09 -0300

    Add solution for L_Sticky_Situation.cpp (Codeforces)

---
+++ Codeforces/L_Sticky_Situation.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+#define pb push_back
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int n; cin >>n;
+    vl a(n); FOR(i,0,n) cin >> a[i];
+    sort(all(a));
+
+    FOR(i,0,n-2){
+        ll m = a[i], sm =a[i+1], maior = a[i+2];
+        if(maior <m + sm){
+            cout <<"possible"<<endl;
+            return;
+        }
+    }
+    cout <<"impossible"<<endl;
+
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
