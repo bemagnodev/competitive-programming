5a1b62785da826f1efbf87160c323a6fe0e6557f
bemagnodev <bemagno13@gmail.com>
2026-01-23 08:42:48 -0300

    Add solution for H_Gray_Code.cpp (Codeforces)

---
+++ Codeforces/H_Gray_Code.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+#define pb push_back
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+void solve() {
+    int n; cin >> n;
+    
+    FOR(i,0,(1<<n)){
+        int f = i ^ (i >> 1);
+        
+        bitset<16> b(f);
+        
+        string s = b.to_string();
+        cout << s.substr(16-n) << endl;
+
+    }
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
