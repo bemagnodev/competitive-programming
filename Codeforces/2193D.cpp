ac3ff3624a1c648f0d115bb3a35455de4ad03f82
bemagnodev <bemagno13@gmail.com>
2026-01-26 16:37:01 -0300

    Add solution for 2193D.cpp (Codeforces)

---
+++ Codeforces/2193D.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+#define pb push_back
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define mii map<int, int>
+
+#define st first
+#define nd second
+const int MOD = 1e9 + 7;  
+const ll INF = 1e18;
+
+
+
+
+void solve() {
+    int n; cin >> n;
+    vi a(n), b(n);
+    FOR(i,0,n) cin >> a[i];
+    FOR(i,0,n) cin >> b[i];
+    
+    sort(a.rbegin(), a.rend());
+    
+    ll ans = 0;
+    ll espadas = 0;
+
+    FOR(i,0,n){
+       espadas += b[i];
+       if(espadas> n) break; // não consegue matar
+       ll  x = a[espadas-1]; // iesima espada mais forte
+       int niveis = i+1;
+       ans = max(ans, niveis*x);
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
