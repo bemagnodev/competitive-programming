69ecd465cb5b55826c8b1a412a1bf01986b5a8f7
bemagnodev <bemagno13@gmail.com>
2026-02-04 18:39:59 -0300

    Add solution for 443A.cpp (Codeforces)

---
+++ Codeforces/443A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define all(x) (x).begin(), (x).end()
+#define deb(k) cerr << #k << ": " << k << "\n";
+#define pb push_back
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+
+const int MOD = 1e9 + 7;  
+const int INF = 2e9;
+const ll INFLL = 4e18;
+
+void solve() {
+    string s; getline(cin , s);
+    set <char> d;
+    for(char c: s){
+        if(c>= 'a' && c<='z'){
+            d.insert(c);
+        }
+    }
+
+    cout << d.size()<< "\n";
+
+}
+
+int main() {
+    fastio;
+    int t =1; 
+    // cin >> t;  
+    while (t--) 
+        solve();  
+    return 0;
+}
